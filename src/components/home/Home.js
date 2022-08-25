51d9f08fb5dad65f33d40c5a42c6bbe5017e7933
abrnsyh <abriansyah1099@gmail.com>
2022-08-25 15:55:10 +0700

    Adding Navbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,12 +3,13 @@ import gambar from "../../assets/background2.svg";
 import gambarrumah from "../../assets/desain1.svg";
 import gambar1 from "../../assets/desain2.svg";
 import gambar2 from "../../assets/desain.svg";
-import "./home.css"
+import { useNavigate } from "react-router-dom";
+import "./home.css";
 const Home = () => {
-  return (
-    <div>
-
-      {/* <div className="carousel-inner" style={{ borderRadius: 20, height: 500, width: 1350 }}>
+	const navigate = useNavigate();
+	return (
+		<div>
+			{/* <div className="carousel-inner" style={{ borderRadius: 20, height: 500, width: 1350 }}>
               <div className="carousel-item active" data-bs-interval="7000">
                 <img src={gambar} alt="..." />
                 <div className="carousel-caption " style={{ position: "absolute", top: "20%", left: "15%", margin: "auto" }}>
@@ -22,36 +23,35 @@ const Home = () => {
               </div>
             </div> */}
 
+			<section className="hero-banner bg-light py-5">
+				<div className="container">
+					<div className="row row align-items-center">
+						<div className="col-lg-5 offset-lg-1 order-lg-1">
+							<img src={gambarrumah} className="img-fluid" alt="Web Development" />
+						</div>
+						<div className="col-lg-6">
+							<h1 className="mt-3">Kita Dekorasi </h1>
+							<p className="lead text-secondary my-5">
+								It is a long established fact that a reader will be distracted by the readable content of a page when
+								looking at its layout. Point of using Lorem Ipsum is that it has a more-or-less normal distribution of
+								letters.
+							</p>
+							<span onClick={() => navigate("categories")} className="btn btn-outline-secondary btn-lg border">
+								Order Now
+							</span>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="quote">
+				<div className="layar-dalam">
+					<p>Selamat datang ke Website Kita Dekor</p>
+				</div>
+			</section>
 
-
-      <section className="hero-banner bg-light py-5">
-        <div className="container">
-          <div className="row row align-items-center">
-            <div className="col-lg-5 offset-lg-1 order-lg-1">
-              <img src={gambarrumah} className="img-fluid" alt="Web Development" />
-            </div>
-            <div className="col-lg-6">
-              <h1 className="mt-3">Kita Dekorasi </h1>
-              <p className="lead text-secondary my-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-              <a href="#" className="btn btn-outline-secondary btn-lg border">Order Now</a>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="quote">
-        <div class="layar-dalam">
-          <p>Selamat datang ke Website Kita Dekor</p>
-        </div>
-      </section>
-
-
-      {/* Card */}
-
-
-
-
-    </div>
-  );
+			{/* Card */}
+		</div>
+	);
 };
 
 export default Home;
