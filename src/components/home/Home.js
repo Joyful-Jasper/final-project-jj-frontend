91156be8d0535632426e6ff4bbccfb4e597fd43d
Baharudin <baharudinfahrul85@gmail.com>
2022-08-25 13:58:37 +0700

    Home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,52 +3,53 @@ import gambar from "../../assets/background2.svg";
 import gambarrumah from "../../assets/desain1.svg";
 import gambar1 from "../../assets/desain2.svg";
 import gambar2 from "../../assets/desain.svg";
-
+import "./home.css"
 const Home = () => {
   return (
-    <div className="countainer">
-      <div className="container-fluid" style={{ backgroundColor: "#ECECEC" }}>
-        <div className="row">
-          <div className="carousel-inner" style={{ borderRadius: 20, height: 500, width: 1350 }}>
-            <div className="carousel-item active" data-bs-interval="7000">
-              <img src={gambar} alt="..." />
-              <div className="carousel-caption " style={{ position: "absolute", top: "20%", left: "15%", margin: "auto" }}>
-                <h1 style={{ color: "white" }} className=" align-self-center d-flex mx-auto ">
-                  Bangun Rumah Impian Anda
-                </h1>
-                <h5 style={{ color: "white" }} className=" align-self-center d-flex mx-auto ">
-                  Marketplace jasa desain & bangun Rumah terpercaya di Indonesia
-                </h5>
+    <div>
+
+      {/* <div className="carousel-inner" style={{ borderRadius: 20, height: 500, width: 1350 }}>
+              <div className="carousel-item active" data-bs-interval="7000">
+                <img src={gambar} alt="..." />
+                <div className="carousel-caption " style={{ position: "absolute", top: "20%", left: "15%", margin: "auto" }}>
+                  <h1 style={{ color: "white" }} className=" align-self-center d-flex mx-auto ">
+                    Bangun Rumah Impian Anda
+                  </h1>
+                  <h5 style={{ color: "white" }} className=" align-self-center d-flex mx-auto ">
+                    Marketplace jasa desain & bangun Rumah terpercaya di Indonesia
+                  </h5>
+                </div>
               </div>
+            </div> */}
+
+
+
+      <section className="hero-banner bg-light py-5">
+        <div className="container">
+          <div className="row row align-items-center">
+            <div className="col-lg-5 offset-lg-1 order-lg-1">
+              <img src={gambarrumah} className="img-fluid" alt="Web Development" />
             </div>
-          </div>
-          <div className="row">
-            <div className="col justify-content-center d-flex mx-auto">
-              <h2 className="my-3"> Populer Projects </h2>
-            </div>
-            <div className="row justify-content-center d-flex mx-auto">
-              <div className="col mx-2 my-2" style={{ backgroundColor: "#30475E" }}>
-                <img src={gambarrumah} height={200} className="mt-3 justify-content-center d-flex mx-auto" />
-                <h4 className="col justify-content-center d-flex mx-auto my-5" style={{ color: "#ECECEC" }}>
-                  Nama Product
-                </h4>
-              </div>
-              <div className="col mx-2 my-2" style={{ backgroundColor: "#30475E" }}>
-                <img src={gambar1} height={200} className="mt-3 justify-content-center d-flex mx-auto" />
-                <h4 className="col justify-content-center d-flex mx-auto my-5" style={{ color: "#ECECEC" }}>
-                  Nama Product
-                </h4>
-              </div>
-              <div className="col mx-2 my-2" style={{ backgroundColor: "#30475E" }}>
-                <img src={gambar2} height={200} className="mt-3 justify-content-center d-flex mx-auto" />
-                <h4 className="col justify-content-center d-flex mx-auto my-5" style={{ color: "#ECECEC" }}>
-                  Nama Product
-                </h4>
-              </div>
+            <div className="col-lg-6">
+              <h1 className="mt-3">Kita Dekorasi </h1>
+              <p className="lead text-secondary my-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+              <a href="#" className="btn btn-outline-secondary btn-lg border">Order Now</a>
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section class="quote">
+        <div class="layar-dalam">
+          <p>Selamat datang ke Website Kita Dekor</p>
+        </div>
+      </section>
+
+
+      {/* Card */}
+
+
+
+
     </div>
   );
 };
